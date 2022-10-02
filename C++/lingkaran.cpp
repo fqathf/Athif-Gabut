c69acd91063aa0ed6498435af0020daa0906d35e
fqathf <100032449+fqathf@users.noreply.github.com>
2022-10-02 08:50:44 +0700

    Tambah C++

---
+++ C++/lingkaran.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double pi = 3.14; //Pi//
+    double d; //Diagram//
+    int r = d / 2; //Jari-jari//
+    
+    cin >> d; //Input diagram//
+    cout << pi * r * r; //Rumus luas lingkaran//
+	cout << pi * 2 * r; //Rumus keliling lingkaran//
+
+    return 0;
+}
