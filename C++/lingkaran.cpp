0fc23234b74cf0c82f507fb2fc7c6869eacb96d0
fqathf <100032449+fqathf@users.noreply.github.com>
2022-10-23 19:36:18 +0700

    Bakso Lingkaran

--- C++/lingkaran.cpp
+++ C++/lingkaran.cpp
@@ -4,12 +4,20 @@ using namespace std;
 int main()
 {
     double pi = 3.14; //Pi//
-    double d; //Diagram//
+    double d; //Diameter//
     int r = d / 2; //Jari-jari//
+
+    //Rumus luas dan keliling lingkaran//
+    double luasLingkaran = pi * r * r;
+    double kelilingLingkaran = pi * 2 * r;
     
-    cin >> d; //Input diagram//
-    cout << pi * r * r; //Rumus luas lingkaran//
-	cout << pi * 2 * r; //Rumus keliling lingkaran//
+    cin >> d; //Input diameter//
+    
+    //Hasil luas lingkaran//
+    cout << "Luas Lingkaran= " << luasLingkaran << "cm^2" << endl;
+    
+    //Hasil keliling lingkaran//
+    cout << "Keliling Lingkaran= " << kelilingLingkaran << "cm";
 
     return 0;
 }
---
+++ JS/lingkaran.js
@@ -0,0 +1,13 @@
+//Variabel
+var pi = 3.14, //Pi
+    d = document.ge, //Diagram
+    r = d / 2; //Jari-Jari
+
+//Rumus luas dan keliling lingkaran
+var luasLingkaran = pi * r * r,
+    kelilingLingkaran = pi * 2 * r;
+
+//Hasil
+console.log("Luas Lingkaran= " + luasLingkaran)
+
+console.log("\nKeliling Lingkaran= " + kelilingLingkaran)
